39f8396f89e94bf9cb27583d03e465596dcdba62
Marcandea <marcandea2@gmail.com>
2017-03-14 11:08:16 -0400

    Primera tarea terminada

--- project.js
+++ project.js
@@ -9,9 +9,24 @@ angular.module('project', ['ngRoute' ])
     description: 'HTML enhanced for web apps!'
   },
   {
-    name: 'Angular',
-    site: 'http://angular.io',
-    description: 'One framework. Mobile and desktop.'
+    precio: 15000,
+    ubicacion: "Zona norte: Av. America - Este",
+    detalles: "Cuenta con una sala, cocina, y 2 baños",
+    lat: -17.374087,
+    lon: -66.138679,
+    tipoP: "Contado",
+    numhab: 3,
+    imagen: "fotos/depa1.jpg"
+  },
+  {
+    precio: 40000,
+    ubicacion: "Zona norte: Av. Simon Lopez - Oeste",
+    detalles: "Cuenta con una sala amplia, muebles de madera, con instalacion de gas y fibra optica",
+    lat: -17.360800,
+    lon: -66.182090,
+    tipoP: "Credito",
+    numhab: 4,
+    imagen: "fotos/depa2.jpg"
   }];
     }
   };
@@ -40,10 +55,25 @@ angular.module('project', ['ngRoute' ])
     description: 'HTML enhanced for web apps!'
   },
   {
-    name: 'Angular',
-    site: 'http://angular.io',
-    description: 'One framework. Mobile and desktop.'
-  }];
+    precio: 15000,
+    ubicacion: "Zona norte: Av. America - Este",
+    detalles: "Cuenta con una sala, cocina, y 2 baños",
+    lat: -17.374087,
+    lon: -66.138679,
+    tipoP: "Contado",
+    numhab: 3,
+    imagen: "fotos/depa1.jpg"
+  },
+  {
+    precio: 40000,
+    ubicacion: "Zona norte: Av. Simon Lopez - Oeste",
+    detalles: "Cuenta con una sala amplia, muebles de madera, con instalacion de gas y fibra optica",
+    lat: -17.360800,
+    lon: -66.182090,
+    tipoP: "Credito",
+    numhab: 4,
+    imagen: "fotos/depa2.jpg"
+  }]; 
 })
  
 .controller('NewProjectController', function($location, projects) {
