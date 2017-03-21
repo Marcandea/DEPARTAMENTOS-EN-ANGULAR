8667b755dab0b6de4a005956b7522a7e8fa8f091
irvin373 <irvin373@gmail.com>
2017-03-21 09:32:37 -0400

    cambios Emergentes

--- project.js
+++ project.js
@@ -3,34 +3,9 @@ angular.module('project', ['ngRoute' ])
 .config(function($routeProvider) {
   var resolveProjects = {
     projects: function () {
-      return [{
-    name: 'AngularJS',
-    site: 'http://angularjs.org',
-    description: 'HTML enhanced for web apps!'
-  },
-  {
-    precio: 15000,
-    ubicacion: "Zona norte: Av. America - Este",
-    detalles: "Cuenta con una sala, cocina, y 2 baños",
-    lat: -17.374087,
-    lon: -66.138679,
-    tipoP: "Contado",
-    numhab: 3,
-    imagen: "fotos/depa1.jpg"
-  },
-  {
-    precio: 40000,
-    ubicacion: "Zona norte: Av. Simon Lopez - Oeste",
-    detalles: "Cuenta con una sala amplia, muebles de madera, con instalacion de gas y fibra optica",
-    lat: -17.360800,
-    lon: -66.182090,
-    tipoP: "Credito",
-    numhab: 4,
-    imagen: "fotos/depa2.jpg"
-  }];
+      return [];
     }
   };
- 
   $routeProvider
     .when('/', {
       controller:'ProjectListController as projectList',
@@ -49,11 +24,7 @@ angular.module('project', ['ngRoute' ])
  
 .controller('ProjectListController', function(projects) {
   var projectList = this;
-  projectList.projects = [{
-    name: 'AngularJS',
-    site: 'http://angularjs.org',
-    description: 'HTML enhanced for web apps!'
-  },
+  projectList.projects = [
   {
     precio: 15000,
     ubicacion: "Zona norte: Av. America - Este",
@@ -78,8 +49,9 @@ angular.module('project', ['ngRoute' ])
  
 .controller('NewProjectController', function($location, projects) {
   var editProject = this;
-  editProject.save = function() {
-      
+  editProject.hello = function() {
+      alert('esto es un mensaje');
   };
+  editProject.msg = "esto es un mensaje";
 }) 
 ;
